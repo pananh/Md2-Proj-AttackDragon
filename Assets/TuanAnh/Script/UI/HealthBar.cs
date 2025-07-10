f1cef7b23f0b064ede7bfe167f9a0e472ba0d175
pananh <pananh@gmail.com>
2025-07-11 00:04:43 +0700

    h 25.07.11 00

--- Assets/TuanAnh/Script/UI/HealthBar.cs
+++ Assets/TuanAnh/Script/UI/HealthBar.cs
@@ -10,12 +10,71 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private UnityEngine.UI.Image hbLeft;
     [SerializeField] private UnityEngine.UI.Image hbRight;
     [SerializeField] private GameObject unit;
-    //private float halfCanvasWidth = 1920f / 2; 
-    //private float halfcanvasHeight = 1080f / 2;
-    private Vector2 viewportPos;
-    private RectTransform healthBarPos;
+    private Vector3 unitLastPos;
+    private Canvas canvas;
+    private RectTransform canvasRec;
+    private RectTransform healthBarRec;
+    private const float aboveYPos = 100f; // Height of the health bar in pixels
 
-    public void ChangeHealth(int health)
+
+    private void Start()
+    {
+        UpdateHealthBar();
+        canvas = GetComponentInParent<Canvas>();
+        canvasRec = canvas.GetComponent<RectTransform>();
+        healthBarRec = GetComponent<RectTransform>();
+        unitLastPos = unit.transform.position;
+
+
+    }
+
+    private void Update()
+    {
+        ChangeHealthByKey();
+      
+    }
+
+    private void LateUpdate()
+    {
+        if (IsUnitNotMove())
+        {
+            return; // If the unit has not moved, skip the update
+        }
+        healthBarRec.anchoredPosition = WorldToCanvasUsingLocalPoint(unit.transform.position, canvasRec);
+    }
+
+    private bool IsUnitNotMove()
+    {
+        if (Vector3.SqrMagnitude(unit.transform.position - unitLastPos) < 0.1f)
+        {
+            unitLastPos = unit.transform.position;
+            return true;
+        }
+        return false;
+    }
+
+    private Vector2 WorldToCanvasUsingLocalPoint(Vector3 unitPos, RectTransform canvasRect)
+    {
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, unitPos); // Tra ve dang toa do theo do phan giai
+        Vector2 localCanvasPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, screenPos, 
+            canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : Camera.main, out localCanvasPos);
+        // Tinh theo toa do Local cua Canvas cha, de che do ScreenSpaceOverlay nen can de null Camera, che do khac thi truyen Camera vao
+        localCanvasPos.y += aboveYPos;
+        return localCanvasPos;
+    }
+
+
+    private Vector2 WorldToCanvas(Vector2 unitPos, RectTransform canvasRect)
+    {
+        Vector2 viewportPos = Camera.main.WorldToViewportPoint(unitPos); // Tra ve dang toa do ty le tu 0 -> 1
+        float canvasWidth = canvasRect.sizeDelta.x;
+        float canvasHeight = canvasRect.sizeDelta.y;
+        float x = (viewportPos.x - 0.5f) * canvasWidth;     // canvas co toa do o giua, nen can tru di nua
+        float y = (viewportPos.y - 0.5f) * canvasHeight + aboveYPos;
+        return new Vector2(x, y);
+    }
+    public void TakeHealth(int health)
     {
         currentHealth = Mathf.Clamp(currentHealth + health, 0, maxHealth);
         UpdateHealthBar();
@@ -29,52 +88,16 @@ public class HealthBar : MonoBehaviour
 
     }
 
-    private void Start()
-    {
-        healthBarPos = GetComponent<RectTransform>();
-        Debug.Log("Canvas Rect: " + healthBarPos.name);
-        UpdateHealthBar();
-    }
-
-    private void Update()
+    private void ChangeHealthByKey()
     {
         if (Input.GetKeyDown(KeyCode.H))
         {
-            ChangeHealth(-10); // Decrease health by 10 when H is pressed
+            TakeHealth(-10); // Decrease health by 10 when H is pressed
         }
         if (Input.GetKeyDown(KeyCode.J))
         {
-            ChangeHealth(10); // Increase health by 10 when J is pressed
+            TakeHealth(10); // Increase health by 10 when J is pressed
         }
-
-        
-
-        Debug.Log("Unit in World: " + unit.transform.position);
-
-        viewportPos = Camera.main.WorldToViewportPoint(unit.transform.position);
-
-        Canvas canvas = GetComponentInParent<Canvas>();
-        RectTransform canvasRectTransform = canvas.GetComponent<RectTransform>();
-
-        Vector2 screenPosition = new Vector2(
-          (viewportPos.x * canvasRectTransform.sizeDelta.x) - (canvasRectTransform.sizeDelta.x * 0.5f),
-          (viewportPos.y * canvasRectTransform.sizeDelta.y) - (canvasRectTransform.sizeDelta.y * 0.5f));
-
-        healthBarPos.anchoredPosition = screenPosition;
-
-        //Debug.Log("Unit in Viewport: " + viewportPos);
-
-
-        //Debug.Log("Screen Width: " + Screen.width + " Screen Height " + Screen.height);
-
-        //Vector2 anchoredPosition = new Vector2(viewportPos.x * Screen.width, viewportPos.y * Screen.height);
-
-        //anchoredPosition.x -= Screen.width / 2; // Centering the health bar
-        //anchoredPosition.y -= Screen.height / 2; // Centering the health bar
-        //healthBarPos.anchoredPosition = anchoredPosition;
-
-        //Debug.Log("Anchored Position: " + healthBarPos.position);
-
 
     }
 
